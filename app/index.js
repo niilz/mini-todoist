7e90d979e0e7fcb7cea248a851df02de985115c5
nilSinger <niilz@outlook.de>
2020-12-22 22:10:39 +0100

    Created virtual list and added first working fetch call that gets the projects from todoist

--- app/index.js
+++ app/index.js
@@ -1,1 +1,40 @@
-console.log('Hello world!');
+import document from "document";
+
+let shoppingList = document.getElementById("shopping-list");
+
+let todoistItems = fetchShoppinList();
+
+shoppingList.delegate = {
+  getTileInfo: (idx) => {
+    return {
+      type: "item-pool",
+      index: idx,
+      name: todoistItems[idx],
+      active: true,
+    };
+  },
+  configureTile: (tile, item) => {
+    if (item.type === "item-pool") {
+      let textEl = tile.getElementById("text");
+      textEl.text = item.name;
+      let touch = tile.getElementById("touchable");
+      touch.onclick = (e) => {
+        textEl.style.fill = item.active ? "red" : "grey";
+        item.active = !item.active;
+      };
+    }
+  },
+};
+
+shoppingList.length = todoistItems.length;
+
+function fetchShoppinList() {
+  return [
+    "Apfelkuchen",
+    "Schokopudding",
+    "Ben&Jerry's",
+    "Rosenkohl",
+    "Knödel",
+    "Rindswurst",
+  ];
+}
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,13 @@
-console.log('Hello world!');
+import { me } from "companion";
+import { token } from "../resources/todoist-token";
+
+let options = {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${token.todoist_token}`,
+    "Content-Type": "application/json",
+  },
+};
+fetch("https://api.todoist.com/rest/v1/projects", options)
+  .then((res) => res.json())
+  .then((json) => console.log("got json", json));
