e61074fea760d369f1b3dd0b67c827fe3d53d4a1
nilSinger <niilz@outlook.de>
2020-12-29 17:21:35 +0100

    Added minimal settings-instructions. Settings are not used yet in the app

--- settings/index.jsx
+++ settings/index.jsx
@@ -6,7 +6,9 @@ registerSettingsPage(({ settings }) => (
           App Settings
         </Text>
       }>
-      <Text>Hello world!</Text>
+      <TextInput label='Enter your todoist API token' settingsKey='api-key'/>
+      <Text>You can access your todoist API token under integrations, in the web-portal of your todoist account:</Text>
+      <Link source='https://todoist.com/prefs/integrations'>https://todoist.com/prefs/integrations</Link>
     </Section>
   </Page>
 ));
